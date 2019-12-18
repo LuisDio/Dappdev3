5e635998246f1a1edde858b74f70427477536ebb
LuisDio <luis.diolindo@gmail.com>
2019-12-18 01:00:15 +0100

    modif for metamask provider

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,9 @@
 import Web3 from 'web3';
 
 // our local copy of web3 taking injected copy of metamask provider 
-const web3 = new Web3(window.web3.currentProvider);
+// const web3 = new Web3(window.web3.currentProvider);
+
+const web3 = new Web3(window.ethereum);
+window.ethereum.enable();
 
 export default web3;
